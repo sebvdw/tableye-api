48203d34220c4d3b0c35129287f9b55428036c65
sebvdw <sebastianvandw@gmail.com>
2024-10-12 21:08:22 +0200

    transaction

--- models/transaction.model.go
+++ models/transaction.model.go
@@ -8,12 +8,12 @@ import (
 
 type Transaction struct {
 	ID            uuid.UUID   `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
-	GameSummaryID uuid.UUID   `gorm:"type:uuid;not null" json:"game_summary_id,omitempty"`
-	GameSummary   GameSummary `gorm:"foreignKey:GameSummaryID" json:"game_summary,omitempty"`
-	PlayerID      uuid.UUID   `gorm:"type:uuid;not null" json:"player_id,omitempty"`
+	GameSummaryID uuid.UUID   `gorm:"type:uuid;not null" json:"-"`
+	GameSummary   GameSummary `gorm:"foreignKey:GameSummaryID" json:"-"`
+	PlayerID      uuid.UUID   `gorm:"type:uuid;not null" json:"-"`
 	Player        Player      `gorm:"foreignKey:PlayerID" json:"player,omitempty"`
 	Amount        float64     `gorm:"not null" json:"amount,omitempty"`
-	Type          string      `gorm:"type:varchar(50);not null" json:"type,omitempty"` // "bet" or "win"
+	Type          string      `gorm:"type:varchar(50);not null" json:"type,omitempty"`
 	CreatedAt     time.Time   `gorm:"not null" json:"created_at,omitempty"`
 	UpdatedAt     time.Time   `gorm:"not null" json:"updated_at,omitempty"`
 }
@@ -28,14 +28,4 @@ type CreateTransactionRequest struct {
 type UpdateTransactionRequest struct {
 	Amount float64 `json:"amount,omitempty"`
 	Type   string  `json:"type,omitempty"`
-}
-
-type TransactionResponse struct {
-	ID            uuid.UUID `json:"id,omitempty"`
-	GameSummaryID uuid.UUID `json:"game_summary_id,omitempty"`
-	PlayerID      uuid.UUID `json:"player_id,omitempty"`
-	Amount        float64   `json:"amount,omitempty"`
-	Type          string    `json:"type,omitempty"`
-	CreatedAt     time.Time `json:"created_at,omitempty"`
-	UpdatedAt     time.Time `json:"updated_at,omitempty"`
 }
