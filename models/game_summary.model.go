4331e1f96b303513cf3a729f7a4c01586b359736
sebvdw <sebastianvandw@gmail.com>
2024-10-12 22:25:46 +0200

    re-do on payload for game summary

--- models/game_summary.model.go
+++ models/game_summary.model.go
@@ -8,11 +8,11 @@ import (
 
 type GameSummary struct {
 	ID           uuid.UUID     `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
-	GameID       uuid.UUID     `gorm:"type:uuid;not null" json:"game_id,omitempty"`
+	GameID       uuid.UUID     `gorm:"type:uuid;not null" json:"-"`
 	Game         Game          `gorm:"foreignKey:GameID" json:"game,omitempty"`
-	CasinoID     uuid.UUID     `gorm:"type:uuid;not null" json:"casino_id,omitempty"`
+	CasinoID     uuid.UUID     `gorm:"type:uuid;not null" json:"-"`
 	Casino       Casino        `gorm:"foreignKey:CasinoID" json:"casino,omitempty"`
-	DealerID     uuid.UUID     `gorm:"type:uuid;not null" json:"dealer_id,omitempty"`
+	DealerID     uuid.UUID     `gorm:"type:uuid;not null" json:"-"`
 	Dealer       Dealer        `gorm:"foreignKey:DealerID" json:"dealer,omitempty"`
 	Players      []Player      `gorm:"many2many:game_players;" json:"players,omitempty"`
 	StartTime    time.Time     `gorm:"not null" json:"start_time,omitempty"`
