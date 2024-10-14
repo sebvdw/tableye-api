81d1532c930a9e7b968a89c86ec836d779b7e620
sebvdw <sebastianvandw@gmail.com>
2024-10-14 23:12:24 +0200

    token change

--- models/auths.model.go
+++ models/auths.model.go
@@ -1,6 +1,5 @@
 package models
 
-// SignInResponse represents the response structure for user sign-in
 type SignInResponse struct {
 	AccessToken string          `json:"access_token"`
 	User        UserResponse    `json:"user"`
--- utils/token.go
+++ utils/token.go
@@ -17,16 +17,20 @@ func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (str
 	if err != nil {
 		return "", fmt.Errorf("create: parse key: %w", err)
 	}
+
 	now := time.Now().UTC()
+
 	claims := make(jwt.MapClaims)
 	claims["sub"] = payload
 	claims["exp"] = now.Add(ttl).Unix()
 	claims["iat"] = now.Unix()
 	claims["nbf"] = now.Unix()
+
 	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
 	if err != nil {
 		return "", fmt.Errorf("create: sign token: %w", err)
 	}
+
 	return token, nil
 }
 
@@ -35,22 +39,27 @@ func ValidateToken(token string, publicKey string) (interface{}, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not decode: %w", err)
 	}
+
 	key, err := jwt.ParseRSAPublicKeyFromPEM(decodedPublicKey)
 	if err != nil {
 		return "", fmt.Errorf("validate: parse key: %w", err)
 	}
+
 	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected method: %s", t.Header["alg"])
 		}
 		return key, nil
 	})
+
 	if err != nil {
 		return nil, fmt.Errorf("validate: %w", err)
 	}
+
 	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 	if !ok || !parsedToken.Valid {
 		return nil, fmt.Errorf("validate: invalid token")
 	}
+
 	return claims["sub"], nil
 }
