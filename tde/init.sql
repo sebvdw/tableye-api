1f383de918cbd4d6bcbe44d21448077f16e606cb
sebvdw <sebastianvandw@gmail.com>
2024-10-15 20:28:02 +0200

    container file update

--- tde/init.sql
+++
@@ -1,5 +0,0 @@
--- Enable encryption for the database
-ALTER DATABASE tableye SET encryption = on;
-
--- You can add other initialization SQL commands here if needed
--- For example, creating tables, indexes, or inserting initial data
